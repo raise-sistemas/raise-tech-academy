86f686d8ba4f7c1ea8f1b5cc11b5fdec266326e5
Neves <marcos.neves@gmail.com>
2021-11-10 10:28:22 -0300

    fix typo

--- denoflix/mod.js
+++ denoflix/mod.js
@@ -4,10 +4,10 @@ import { listarGeneros } from "./menus/listarGeneros.js";
 const menu = [
   ["Listar Gêneros", listarGeneros],
   ["Filmes em Cartaz", null], // https://developers.themoviedb.org/3/movies/get-now-playing Matheus/Leonardo Guaraná
-  ["Em Breve nos Cinemas", null] // https://developers.themoviedb.org/3/movies/get-upcoming Jorge/Nicole
-  ["Melhores Avaliados", null] // https://developers.themoviedb.org/3/movies/get-top-rated-movies Felipe/Sabrina
-  ["Mais Populares", null] // https://developers.themoviedb.org/3/movies/get-popular-movies Paulo/Leonardo Lima
-  ["Mais Recentes", null] // https://developers.themoviedb.org/3/movies/get-latest-movie Otávio/João
+  ["Em Breve nos Cinemas", null], // https://developers.themoviedb.org/3/movies/get-upcoming Jorge/Nicole
+  ["Melhores Avaliados", null], // https://developers.themoviedb.org/3/movies/get-top-rated-movies Felipe/Sabrina
+  ["Mais Populares", null], // https://developers.themoviedb.org/3/movies/get-popular-movies Paulo/Leonardo Lima
+  ["Mais Recentes", null], // https://developers.themoviedb.org/3/movies/get-latest-movie Otávio/João
   ["Filmes em Alta", null], // https://developers.themoviedb.org/3/trending/get-trending movie/day Patrick/Guilherme
 ];
 
