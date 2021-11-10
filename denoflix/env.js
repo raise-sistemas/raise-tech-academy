1fd37ec5c1d3da34fddece3a348944fe3a31c93f
Matheus Matsukawa <matheusmatsu@gmail.com>
2021-11-10 16:26:19 -0300

    Add LANG_PTBR and fix BASE_URL. Co-authored-by: Leoguarana <Leoguarana@users.noreply.github.com>

--- denoflix/env.js
+++ denoflix/env.js
@@ -1,3 +1,5 @@
 export const API_KEY = "f7935dbaf15738f81daed04b531c65fd";
 
-export const BASE_URL = "https://developers.themoviedb.org/3/";
+export const BASE_URL = "https://api.themoviedb.org/3/";
+
+export const LANG_PTBR = "language=pt-BR"
