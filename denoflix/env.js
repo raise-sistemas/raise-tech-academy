96b15337389cbb5dba05e4fa96bdfb72792ff1cd
Neves <marcos.neves@gmail.com>
2021-11-10 10:30:58 -0300

    add env file

---
+++ denoflix/env.js
@@ -0,0 +1,1 @@
+export const API_KEY = "f7935dbaf15738f81daed04b531c65fd";
--- denoflix/menus/listarGeneros.js
+++ denoflix/menus/listarGeneros.js
@@ -1,6 +1,8 @@
+import { API_KEY } from "../env.js";
+
 export async function listarGeneros() {
   const url =
-    "https://api.themoviedb.org/3/genre/movie/list?api_key=f7935dbaf15738f81daed04b531c65fd&language=pt-BR";
+    `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=pt-BR`;
 
   const resposta = await fetch(url);
   const json = await resposta.json();
