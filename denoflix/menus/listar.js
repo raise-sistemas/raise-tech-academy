683340677b5d9b0eab9a387b96d71184a0951b37
Matheus Matsukawa <matheusmatsu@gmail.com>
2021-11-11 14:01:06 -0300

    Adiciona método listar
    Co-authored-by: Leoguarana <Leoguarana@users.noreply.github.com>

---
+++ denoflix/menus/listar.js
@@ -0,0 +1,31 @@
+/* 
+  A função listar recebe como argumentos obrigatórios a url(sem paginação), uma callback
+  e dois parametros opcionais de paginação init e end definem a primeira e a última página
+  a ser chamada.
+  A callback recebe como parametro um array de objetos resultante da chamada da API
+  e deve retornar um array(ou matriz para múltiplas tabelas) de objetos com apenas os
+  dados que se deseja imprimir no console.table como atributos.
+  Exemplo de implementação em menus/listarCartaz.js
+   
+*/
+
+export async function listar(url, callback, init= 1, end= init) {  //
+  const result = [];
+  
+  for(let current = init; current <= end; current++) {
+    const curl = url + `&page=${init}`;
+    const res = await fetch(curl);
+    const json = await res.json();
+    result.push(...json.results);
+  }
+
+  const data = callback(result);
+
+  if(!data[0][0]) {
+    console.table(data);
+  } else {
+    data.forEach(element => {
+      if(element.length) console.table(element);
+    });
+  }
+}
