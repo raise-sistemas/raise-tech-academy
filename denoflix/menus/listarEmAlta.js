cc57c61d915418eeee1df2b88d2bfe67143286b4
Patrick Melo <patrickabimael@yahoo.com.br>
2021-11-11 17:40:03 -0300

    adicionando função iniFetch

--- denoflix/menus/listarEmAlta.js
+++ denoflix/menus/listarEmAlta.js
@@ -1,11 +1,13 @@
 import { API_KEY, BASE_URL } from '../env.js'
+import { initFetch } from '../utils/initFetch.js'
 
 export async function listarEmAlta() {
   const url = `${BASE_URL}trending/movie/day?api_key=${API_KEY}`
-  const resposta = await fetch(url)
-  const json = await resposta.json()
+  const json = await initFetch(url)
   const emAlta = json.results.map(emAlta => emAlta.title)
 
   console.table(emAlta)
   return emAlta
 }
+
+listarEmAlta()
