885b0913be4b2373de4bc8c803d8beb0ff929ba2
Patrick Melo <patrickabimael@yahoo.com.br>
2021-11-11 11:00:40 -0300

    criando função listarEmAlta

--- denoflix/menus/listarEmAlta.js
+++ denoflix/menus/listarEmAlta.js
@@ -1,1 +1,13 @@
-console.log('teste3')
+import { API_KEY, BASE_URL } from '../env.js'
+
+export async function listarEmAlta() {
+  const url = `${BASE_URL}trending/movie/day?api_key=${API_KEY}`
+  const resposta = await fetch(url)
+  const json = await resposta.json()
+  const emAlta = json.results.map(emAlta => emAlta.title)
+
+  console.table(emAlta)
+  return emAlta
+}
+
+listarEmAlta()
