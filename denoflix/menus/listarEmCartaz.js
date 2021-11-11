016b1c14d326ab852d98c9458f442f86612083dc
Matheus Matsukawa <matheusmatsu@gmail.com>
2021-11-11 14:06:10 -0300

    Separa os resultados de busca e abrange mais atributos. Co-authored-by: Leoguarana <Leoguarana@users.noreply.github.com>

--- denoflix/menus/listarEmCartaz.js
+++ denoflix/menus/listarEmCartaz.js
@@ -1,10 +1,25 @@
 import { API_KEY, BASE_URL, LANG } from "../env.js";
+import { listar } from "../menus/listar.js";
 
-export async function listarEmCartaz() {
-  const url = `${BASE_URL}movie/now_playing?api_key=${API_KEY}&language=${LANG}`;
+const url = `${BASE_URL}movie/now_playing?api_key=${API_KEY}&language=${LANG}`;
 
-  const resposta = await fetch(url);
-  const json = await resposta.json();
-  const emCartaz = json.results.map((movie) => movie.title);
-  console.table(emCartaz);
-}
+export const listarEmCartaz = () =>  listar(url, (result) => {
+  const filteredResults = [[],[]];  // Matriz a ser retornada, pois serão geradas duas tabelas.
+
+  result.forEach((element) => {
+    const obj = {
+      title: element.title,      
+      original_language: element.original_language,
+      isadult: element.adult,
+      average: element.vote_average,
+    }
+
+    if(element.adult) { // filtra os filmes adultos e não adultos para serem imprimidos em tabelas diferentes.
+      filteredResults[1].push(obj);
+    } else {
+      filteredResults[0].push(obj);
+    }
+  });
+    
+    return filteredResults;
+}, 1, 2);
