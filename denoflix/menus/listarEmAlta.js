07f2a20fd58afab4dcefff1173655ba338746e82
Patrick Melo <patrickabimael@yahoo.com.br>
2021-11-11 21:16:01 -0300

    atualizando em alta e criando função de filmes adultos

--- denoflix/menus/listarEmAlta.js
+++ denoflix/menus/listarEmAlta.js
@@ -1,13 +1,25 @@
 import { API_KEY, BASE_URL } from '../env.js'
 import { initFetch } from '../utils/initFetch.js'
+import { listar } from '../menus/listar.js'
+import { isAdult } from './filmesAdultos.js'
 
 export async function listarEmAlta() {
   const url = `${BASE_URL}trending/movie/day?api_key=${API_KEY}`
-  const json = await initFetch(url)
-  const emAlta = json.results.map(emAlta => emAlta.title)
+  const response = await initFetch(url)
+  const movies = response.results.map(element => element.title)
+  const filmes = []
 
-  console.table(emAlta)
-  return emAlta
+  response.results.forEach(element => {
+    filmes.push({
+      title: element.title,
+      vote_average: element.vote_average,
+      release_date: element.release_date
+    })
+  })
+
+  console.table(isAdult(filmes))
+
+  return movies
 }
 
 listarEmAlta()
