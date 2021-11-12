73f3eb4699d96e6f3fad4a8e4867f30f9ad5588e
Otávio Jr. O. Nunes <OtavioJr08@gmail.com>
2021-11-12 17:12:13 -0300

    Implementar busca na API com Promises (then e catch) no arquivo listarMaisRecente.js

--- denoflix/menus/listarMaisRecente.js
+++ denoflix/menus/listarMaisRecente.js
@@ -12,14 +12,47 @@ export async function listarMaisRecente() {
     };
   });
 
-  const movie = {
+  const filme = {
     'Título': resposta.title,
     'Status': resposta.status,
     'Adulto': (resposta.adulto) ? 'Sim' : 'Não',
-    'Página no IMDB': (resposta.imdb_id) ? 'https://www.imdb.com/title/' + resposta.imdb_id : 'Indisponível',
+    'Página no IMDB': (resposta.imdb_id) ? `https://www.imdb.com/title/${resposta.imdb_id}` : 'Indisponível',
     'Idioma': (linguagem.length) ? linguagem[0].idioma : 'Indisponível',
     'Código idioma': (linguagem.length) ? linguagem[0].codigo : 'Indisponível',
   }
 
-  console.table([movie]);
+  console.table([filme]);
 }
+
+/*
+  //Implementação utilizando Promises
+
+  export function listarMaisRecente() {
+    const url = `${BASE_URL}movie/latest?api_key=${API_KEY}&language=${LANG}`;
+
+    initFetch(url)
+    .then(resposta => resposta.json())
+    .then(dados => {
+      if(!dados.spoken_languages)
+        throw 'Erro ao consultar API';
+
+      const linguagem = dados.spoken_languages.map(lang => {
+        return {
+          idioma: lang.english_name,
+          codigo: lang.iso_639_1
+        };
+      });
+
+      const filme = {
+        'Título': dados.title,
+        'Status': dados.status,
+        'Adulto': (dados.adulto) ? 'Sim' : 'Não',
+        'Página no IMDB': (dados.imdb_id) ? 'https://www.imdb.com/title/' + dados.imdb_id : 'Indisponível',
+        'Idioma': (linguagem.length) ? linguagem[0].idioma : 'Indisponível',
+        'Código idioma': (linguagem.length) ? linguagem[0].codigo : 'Indisponível',
+      }
+
+      console.table([filme]);
+    }).catch(erro => console.error(erro));
+  }
+*/
