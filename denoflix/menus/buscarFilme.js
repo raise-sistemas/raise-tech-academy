77e4df8e966ce3c1890e1284f139f08e003dbf4a
Matheus Matsukawa <matheusmatsu@gmail.com>
2021-11-12 15:22:59 -0300

    refatora o código e adiciona formatação de data e ordenação por nota

--- denoflix/menus/buscarFilme.js
+++ denoflix/menus/buscarFilme.js
@@ -1,16 +1,22 @@
 import { API_KEY, BASE_URL, LANG } from "../env.js"
 import { listar } from "../menus/listar.js"
+import { formatDate } from "../utils/formatDate.js"
+import { compareAverage } from "../utils/compareAverage.js"
 
 export const buscarFilme = () => {
     const escolha = prompt("---digite o filme que deseja pesquisar---\n")
-    const url = `${BASE_URL}search/movie?api_key=${API_KEY}&language=${LANG}&query=${escolha}`
+    const url = `${BASE_URL}search/movie?api_key=${API_KEY}&language=${LANG}&query=${escolha}&include_adult=true`
     
-    return listar(url,(result)=> result.map(result => {
+    return listar(url, (result) => {
         return {
-                title: result.title,      
-                original_language: result.original_language,
-                release: result.release_date,
-                average: result.vote_average,
-        }   
-    }))
+            search_result: result.map(entry => {
+                return {
+                    title: entry.title, 
+                    original_language: entry.original_language,
+                    vote_average: entry.vote_average,
+                    release_date: formatDate(entry.release_date)
+                }   
+            }).sort(compareAverage)
+        }
+    })
 }
