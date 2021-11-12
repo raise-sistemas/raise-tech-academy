446100becfc0957fc7b5336d23495a0d2be40286
Matheus Matsukawa <matheusmatsu@gmail.com>
2021-11-12 15:20:03 -0300

    muda a implementação e o tipo de retorno da callback

--- denoflix/menus/listar.js
+++ denoflix/menus/listar.js
@@ -8,25 +8,31 @@
   Exemplo de implementação em menus/listarCartaz.js
    
 */
+import { initFetch } from '../utils/initFetch.js'
 
 export async function listar(url, callback, init = 1, end = init) {
-  //
   const result = []
 
   for (let current = init; current <= end; current++) {
     const curl = url + `&page=${current}`
-    const res = await fetch(curl)
-    const json = await res.json()
+    const json = await initFetch(curl)
     result.push(...json.results)
   }
 
   const data = callback(result)
-
-  if (!data[0][0]) {
-    console.table(data)
-  } else {
-    data.forEach(element => {
-      if (element.length) console.table(element)
-    })
+  
+  for(const entry in data) {
+    if(data[entry].length > 0) {
+      console.log(toTitleCase(entry.replace("_"," ")))
+      console.table(data[entry])
+    }
   }
+}
+
+function toTitleCase(str) {
+  const arr = str.split(" ")
+  const titledArr = arr.map(word => 
+    word.charAt(0).toUpperCase() + word.slice(1))
+    
+  return titledArr.join(" ")
 }
