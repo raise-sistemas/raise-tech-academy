681eccc70f45f3ab7220fa12bc2ec9d921ff4898
Patrick Melo <patrickabimael@yahoo.com.br>
2021-11-12 00:08:27 -0300

    correção de referência da variável

--- denoflix/menus/listar.js
+++ denoflix/menus/listar.js
@@ -9,23 +9,24 @@
    
 */
 
-export async function listar(url, callback, init= 1, end= init) {  //
-  const result = [];
-  
-  for(let current = init; current <= end; current++) {
-    const curl = url + `&page=${init}`;
-    const res = await fetch(curl);
-    const json = await res.json();
-    result.push(...json.results);
+export async function listar(url, callback, init = 1, end = init) {
+  //
+  const result = []
+
+  for (let current = init; current <= end; current++) {
+    const curl = url + `&page=${current}`
+    const res = await fetch(curl)
+    const json = await res.json()
+    result.push(...json.results)
   }
 
-  const data = callback(result);
+  const data = callback(result)
 
-  if(!data[0][0]) {
-    console.table(data);
+  if (!data[0][0]) {
+    console.table(data)
   } else {
     data.forEach(element => {
-      if(element.length) console.table(element);
-    });
+      if (element.length) console.table(element)
+    })
   }
 }
