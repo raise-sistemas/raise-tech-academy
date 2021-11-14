9ee64f6599502446192f1fa3245f4ccf597199aa
Matheus Matsukawa <matheusmatsu@gmail.com>
2021-11-14 17:55:02 -0300

    adiciona curring na declaração da função para chamada mais legível Co-authored-by: Leoguarana <Leoguarana@users.noreply.github.com>

--- denoflix/menus/listar.js
+++ denoflix/menus/listar.js
@@ -1,16 +1,6 @@
-/* 
-  A função listar recebe como argumentos obrigatórios a url(sem paginação), uma callback
-  e dois parametros opcionais de paginação init e end definem a primeira e a última página
-  a ser chamada.
-  A callback recebe como parametro um array de objetos resultante da chamada da API
-  e deve retornar um array(ou matriz para múltiplas tabelas) de objetos com apenas os
-  dados que se deseja imprimir no console.table como atributos.
-  Exemplo de implementação em menus/listarCartaz.js
-   
-*/
 import { initFetch } from '../utils/initFetch.js'
 
-export async function listar(url, callback, init = 1, end = init) {
+export const listar = (url, init = 1, end = init) => async (callback) => {
   const result = []
 
   for (let current = init; current <= end; current++) {
@@ -22,17 +12,19 @@ export async function listar(url, callback, init = 1, end = init) {
   const data = callback(result)
   
   for(const entry in data) {
-    if(data[entry].length > 0) {
+    if(data[entry]?.length && Array.isArray(data[entry])) {
       console.log(toTitleCase(entry.replace("_"," ")))
       console.table(data[entry])
     }
   }
+  
 }
 
 function toTitleCase(str) {
   const arr = str.split(" ")
   const titledArr = arr.map(word => 
     word.charAt(0).toUpperCase() + word.slice(1))
-    
+
   return titledArr.join(" ")
 }
+
