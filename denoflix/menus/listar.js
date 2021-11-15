380bd38a3395ee597180c14cc82a901e8c1c34ee
Matheus Matsukawa <matheusmatsu@gmail.com>
2021-11-14 23:12:31 -0300

    adiciona método alternativo com promises

--- denoflix/menus/listar.js
+++ denoflix/menus/listar.js
@@ -20,6 +20,7 @@ export const listar = (url, init = 1, end = init) => async (callback) => {
   
 }
 
+// Função Auxiliar
 function toTitleCase(str) {
   const arr = str.split(" ")
   const titledArr = arr.map(word => 
@@ -28,3 +29,43 @@ function toTitleCase(str) {
   return titledArr.join(" ")
 }
 
+
+// Implementação com Promises(não funcionando da maneira esperada)
+
+/*
+export const listar = (url, init = 1, end = init) => (callback) => {
+  getPages(url, init, end)
+    .then(arr => callback(arr))
+    .then(movies => {
+      for(const entry in movies) {
+        if(movies[entry]?.length && Array.isArray(movies[entry])) {
+          console.log(toTitleCase(entry.replace("_"," ")))
+          console.table(movies[entry])
+        }
+      }
+    }).catch(err => console.log(e))
+    
+}
+
+
+function getPage(url) {
+  return new Promise(resolve => {
+    fetch(url)
+      .then(rawData => {
+        if(!rawData) throw "Erro ao requisitar os dados da API"
+        return rawData.json()
+      })
+      .then(page => resolve(...page.results))
+  })
+}
+
+function getPages(url, init, end) {
+  const pages = []
+  for (let current = init; current <= end; current++) {
+    const curl = url + `&page=${current}`
+    pages.push(getPage(curl))
+  }
+  return Promise.allSettled(pages)
+
+}
+*/
