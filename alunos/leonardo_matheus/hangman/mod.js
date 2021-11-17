b0f5fdb80932bec0f8892482860cc2a6e453e8f1
Matheus Matsukawa <matheusmatsu@gmail.com>
2021-11-17 18:35:18 -0300

    change import path

--- alunos/leonardo_matheus/hangman/mod.js
+++ alunos/leonardo_matheus/hangman/mod.js
@@ -1,9 +1,12 @@
 import { pipe } from "./utils/pipe.js"
 import { character } from "./assets/character.js"
-import { getSecretWord } from "./game/getSecretWord.js"
-import { init } from "./game/init.js"
-import { display } from "./game/display.js"
-import { getInput } from "./game/getInput.js"
+import {
+  getSecretWord, 
+  init, 
+  display, 
+  getInput 
+} from "./game/index.js"
+
 const run = pipe()
 
 run(
