f832fb0d77cc0e4d7e9db550c412c7481c62a9ad
Matheus Matsukawa <matheusmatsu@gmail.com>
2021-11-17 18:34:53 -0300

    add folders index
    Co-authored-by: Leoguarana <Leoguarana@users.noreply.github.com>

---
+++ alunos/leonardo_matheus/hangman/game/index.js
@@ -0,0 +1,11 @@
+import { display } from "./display.js"
+import { getInput } from "./getInput.js"
+import { getSecretWord } from "./getSecretWord.js"
+import { init } from "./init.js"
+
+export {
+  display,
+  getInput,
+  getSecretWord,
+  init
+}
