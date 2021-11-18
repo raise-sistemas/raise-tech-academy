c0e60d3d1e193fcfc95e06263e2320cfaa8167ac
Matheus Matsukawa <matheusmatsu@gmail.com>
2021-11-17 21:21:33 -0300

    add imports
    Co-authored-by: Leoguarana <Leoguarana@users.noreply.github.com>

--- alunos/leonardo_matheus/hangman/game/index.js
+++ alunos/leonardo_matheus/hangman/game/index.js
@@ -2,10 +2,14 @@ import { display } from "./display.js"
 import { getInput } from "./getInput.js"
 import { getSecretWord } from "./getSecretWord.js"
 import { init } from "./init.js"
+import { checkInput } from "./checkInput.js"
+import { updateState } from "./updateState.js"
 
 export {
   display,
   getInput,
   getSecretWord,
-  init
+  init,
+  checkInput,
+  updateState
 }
