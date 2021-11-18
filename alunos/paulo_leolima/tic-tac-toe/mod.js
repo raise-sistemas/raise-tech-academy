95764b885031e524add98902dcea6b903d636f34
Paulo Jr. Niche <pjniche@users.noreply.github.com>
2021-11-17 21:21:57 -0300

    Paulo / Leo Lima - tic-tac-toe iniciado.
    Co-authored-by: Leonardo Lima <LeozinLL@users.noreply.github.com>

---
+++ alunos/paulo_leolima/tic-tac-toe/mod.js
@@ -0,0 +1,26 @@
+import { log } from "./log.js";
+
+const EMPTY = " ";
+const PLAYER1 = "X";
+const PLAYER2 = "O";
+const board = [
+  [EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY],
+];
+
+function drawBoard(board) {
+  console.clear();
+  console.table(board);
+}
+while (EMPTY == " ") {
+let currentPlayer = PLAYER1;
+board[1][1] = currentPlayer;
+drawBoard(board);
+currentPlayer = PLAYER2;
+const input = prompt(`Jogador: ${currentPlayer}. Digite a linha e coluna:`);
+const row = +input.charAt(0);
+const col = +input.charAt(1);
+board[row][col] = currentPlayer;
+drawBoard(board);
+}
