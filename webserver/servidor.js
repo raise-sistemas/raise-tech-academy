aeff94290a6b61becdf53a950d187006fa65f5bd
Neves <marcos.neves@gmail.com>
2021-11-18 11:49:37 -0300

    corrigir conexao bloqueada pelo await

--- webserver/servidor.js
+++ webserver/servidor.js
@@ -2,12 +2,17 @@ const listener = Deno.listen({ port: 8000 });
 console.clear();
 console.log("listening on port", listener.addr.port);
 
-for await (const conn of listener) {
-  console.log("Nova Conexão!");
+async function handleConnection(conn) {
   const httpConn = Deno.serveHttp(conn);
   for await (const request of httpConn) {
     console.log("Novo Request");
     const response = new Response("Meu Primeiro Servidor!");
     request.respondWith(response);
   }
+}
+
+for await (const conn of listener) {
+  console.log("Nova Conexão!");
+  // se colocar await abaixo, trava novas conexões
+  handleConnection(conn);
 }
