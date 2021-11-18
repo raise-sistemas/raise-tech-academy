c4420413283110cd3de45000c83cea1ef3e79748
Matheus Matsukawa <matheusmatsu@gmail.com>
2021-11-18 12:34:34 -0300

    add missing "E" letter treatment

--- alunos/leonardo_matheus/hangman/utils/specialChar.js
+++ alunos/leonardo_matheus/hangman/utils/specialChar.js
@@ -4,5 +4,6 @@ export const specialChar = (str) => {
     .replace(/Õ|Ô|Ò|Ó/, "O")
     .replace(/Ũ|Û|Ú|Ù|Ü/, "U")
     .replace(/Í|Ì|Ĩ|Î/, "I")
+    .replace(/Ẽ|Ê|É|È/, "E")
     .replace(/Ç/,"C")
 }
