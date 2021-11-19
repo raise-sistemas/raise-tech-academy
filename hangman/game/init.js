c9716600f59749eb4641d6efc7114f693f5a6deb
Matheus Matsukawa <matheusmatsu@gmail.com>
2021-11-19 20:58:44 -0300

    refactorate

--- hangman/game/init.js
+++ hangman/game/init.js
@@ -1,18 +1,13 @@
-export const init = async (secretWord, getTips = () => {}) => {
+export const init = (secretWord, tips) => {
   const correct = new Array(secretWord.length)
   correct.fill(" ")
-  const tips = await getTips(secretWord)
-  console.log(tips)
   return {
     secretWord: secretWord,
     status: "playing",
     chances: 6,
     correct: correct,
     wrong: [],
-    tips: {
-      class: tips ? tips.class : [],
-      synonyms: tips ? tips.synonyms : []
-    }
+    tips: tips
   }
   
 }
