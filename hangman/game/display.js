6dcbfb98d6468947b1ba6e023c67158828ed45a0
Matheus Matsukawa <matheusmatsu@gmail.com>
2021-11-19 00:02:04 -0300

    add tips feature

--- hangman/game/display.js
+++ hangman/game/display.js
@@ -1,4 +1,4 @@
-export const display = (gameState, character, log) => {
+export const display = async (gameState, character, log) => {
   
   log(character[6 - gameState.chances])
   
@@ -15,6 +15,35 @@ export const display = (gameState, character, log) => {
     log("Letras erradas: " + gameState.wrong.join(" "))
   }
   
+  //tips
+  if(gameState.chances < 3){
+    log()
+    log("Dicas:")
+    log(`Classe gramatical: ${gameState.tips.class}`)   
+  }
+  
+  if(gameState.chances < 2){
+    const count = 
+      gameState.tips.synonyms.length > 5 ?  
+        5 : 
+        gameState.tips.synonyms.length
+    const synonymsArr = gameState.tips.synonyms.slice(0, count)
+    const synonyms = synonymsArr.join(" \| ")
+    log(`Sinônimos: ${synonyms}`)
+  }
+  
+  // End Game
+  if(gameState.status !== "playing"){
+    log()
+    if(gameState.status === "Game Over") {
+      log("%c" + gameState.status, "color: red;")
+      log()
+      log(`A palavra secreta era: ${gameState.secretWord}`)
+    }
+    if(gameState.status === "Victory") {
+      log("%c" + gameState.status, "color: green;")
+    }
+  }
 
   return gameState
 }
