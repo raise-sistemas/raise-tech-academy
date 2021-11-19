5d672075119b15033140b6d3c3a08b32c2e3ee97
Matheus Matsukawa <matheusmatsu@gmail.com>
2021-11-19 00:01:13 -0300

    add tips feature

--- hangman/game/init.js
+++ hangman/game/init.js
@@ -1,12 +1,18 @@
-export const init = (secretWord) => {
+export const init = async (secretWord, getTips = () => {}) => {
   const correct = new Array(secretWord.length)
   correct.fill(" ")
-
+  const tips = await getTips(secretWord)
+  console.log(tips)
   return {
     secretWord: secretWord,
     status: "playing",
     chances: 6,
     correct: correct,
-    wrong: []
+    wrong: [],
+    tips: {
+      class: tips.class,
+      synonyms: tips.synonyms
+    }
   }
+  
 }
