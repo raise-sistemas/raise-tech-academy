46fb03713900e51d57b7dd7b83a953642eb93688
Matheus Matsukawa <matheusmatsu@gmail.com>
2021-11-19 21:00:05 -0300

    refactorate

--- hangman/mod.js
+++ hangman/mod.js
@@ -1,16 +1,10 @@
-import { pipe } from "./utils/pipe.js"
-import { tips } from "./utils/tips.js"
-import {
-  getSecretWord, 
-  init,
-} from "./game/index.js"
+import { getSecretWord, getTips, init, endGame } from "./game/index.js"
 import { run } from "./run.js"
 
+const word = getSecretWord()
+const tips = await getTips(word)
+const newGame = init(word, tips)
 
-const game = await pipe()(
-  getSecretWord,
-  secretWord => init(secretWord, tips),
-  newGame => run(newGame, console.log, prompt, console.clear)
-)
+const game = run(newGame, console.log, prompt, console.clear)
 
-
+endGame(game, console.log)
