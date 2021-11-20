87ab1a84106614d63741bd4b49e5a62c3f98f034
Matheus Matsukawa <matheusmatsu@gmail.com>
2021-11-19 21:08:25 -0300

    pass words array as a parameter to getSecretWord

--- hangman/game/getSecretWord.js
+++ hangman/game/getSecretWord.js
@@ -1,5 +1,4 @@
-import words from "../assets/words.js"
-export const  getSecretWord = () => {
+export const  getSecretWord = (words) => {
   const index = Math.round(Math.random() * words.length);
   return words[index].toUpperCase();
 }
--- hangman/mod.js
+++ hangman/mod.js
@@ -1,7 +1,8 @@
+import words from "./assets/words.js"
 import { getSecretWord, getTips, init, endGame } from "./game/index.js"
 import { run } from "./run.js"
 
-const word = getSecretWord()
+const word = getSecretWord(words)
 const tips = await getTips(word)
 const newGame = init(word, tips)
 
