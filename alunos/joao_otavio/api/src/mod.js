4eaa13bb4deaa604234e5cb564b052fee91362d4
Otávio Jr. O. Nunes <OtavioJr08@gmail.com>
2021-11-21 19:53:30 -0300

    Fazer API

--- alunos/joao_otavio/api/src/mod.js
+++ alunos/joao_otavio/api/src/mod.js
@@ -1,8 +1,7 @@
 import { serve } from 'https://deno.land/std@0.114.0/http/server.ts';
-import { routes } from './routes.js';
+import { routes } from './router.js';
 
 await serve((request) => {
-  console.log('Nova conexão');
-  const url = new URL(request.url);
-  return routes(url);
+  console.log('Nova conexão');  
+  return routes(request);
 });
