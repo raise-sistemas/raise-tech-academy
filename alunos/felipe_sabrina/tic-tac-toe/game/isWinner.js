dd032263fb955b21505dfafe9d67eb2e0e542479
Paulo Jr. Niche <pjniche@gmail.com>
2021-11-22 13:55:49 -0300

    Trocar 1 e 0 por true e false - OK!

--- alunos/felipe_sabrina/tic-tac-toe/game/isWinner.js
+++ alunos/felipe_sabrina/tic-tac-toe/game/isWinner.js
@@ -1,14 +1,26 @@
-//ok
-import { WIN } from "../utils/general.js";
 import { PLAYER1 } from "../utils/general.js";
 import { PLAYER2 } from "../utils/general.js";
+
+const WIN = [
+  // de lado
+  [0, 0, 0, 1, 0, 2],
+  [1, 0, 1, 1, 1, 2],
+  [2, 0, 2, 1, 2, 2],
+  //em pe
+  [0, 0, 1, 0, 2, 0],
+  [0, 1, 1, 1, 2, 1],
+  [0, 2, 1, 2, 2, 2],
+  //diagonais
+  [0, 0, 1, 1, 2, 2],
+  [0, 2, 1, 1, 2, 0],
+];
 
 export let playerOneValue = 0;
 export let playerTwoValue = 0;
 
 // renomear a função para deixar claro o que ela realmente faz
 // separar a verificação de quem ganhou da modificação do score
-// Trocar 1 e 0 por true e false
+// Trocar 1 e 0 por true e false - OK!
 export function isWinner(currentPlayer, board) {
   let result = false;
   WIN.forEach((element) => {
@@ -17,6 +29,8 @@ export function isWinner(currentPlayer, board) {
       board[element[2]][element[3]] === currentPlayer &&
       board[element[4]][element[5]] === currentPlayer
     ) {
+
+
       if (currentPlayer === PLAYER1) {
         //console.log("entrei");
         // Retirar essa mutação para um local mais claro
@@ -26,12 +40,14 @@ export function isWinner(currentPlayer, board) {
         // Retirar essa mutação para um local mais claro
         playerTwoValue++;
       }
+
+
       console.log(`${currentPlayer} - Venceu o jogo!!!\n`);
       result = true;
-      return 1;
+      return true;
     }
   });
-  if (result) return 1;
+  if (result) return true;
 
-  return 0;
+  return false;
 }
