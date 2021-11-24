da62700efe6fe6c4ad5f9298508c16a6db0b4494
Paulo Jr. Niche <pjniche@gmail.com>
2021-11-24 18:57:09 -0300

    Refatorando mod.js

---
+++ tarefas/todo-cli/drawMenu.js
@@ -0,0 +1,21 @@
+import { cliMenu } from "./utils/cliMenu.js";
+
+// Cada entrada do menu é um array com o título da entrada na posição 0 e a referência
+// a função na posição 1
+/*  Exemplo:(mesma maneira utilizada no projeto denoflix)
+  const menu = [
+    ["titulo1", funçao1],
+    ["titulo2", função2],
+    ...
+    ["tituloN", funçãoN]
+  ]
+*/
+export async function drawMenu() {
+  const menu = [
+    //entradas de teste, o primeiro a adicionar as funções pode remover os foo
+    ["Remover TO-DO", () => console.log("foo1")], // Add função.
+    ["foo2", () => console.log("foo2")],
+  ];
+
+  while (await cliMenu("  --==<[ TO-DO Menu ]>==--", menu));
+}
