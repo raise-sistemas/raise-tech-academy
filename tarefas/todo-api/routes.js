0ed44c889c865b2e64d7e2b651777d34b24f594c
Neves <marcos.neves@gmail.com>
2021-11-25 08:57:16 -0300

    deno version

--- tarefas/todo-api/routes.js
+++ tarefas/todo-api/routes.js
@@ -1,14 +1,22 @@
 import { Router } from "./router.js";
-import * as actions from "./actions.js";
+// import * as actions from "./actions.js";
 
 export const routes = new Router();
-
-routes.get("/", actions.rootAction);
-routes.get("/routes", actions.routesAction);
-routes.get("/todos", actions.listTodosAction);
-routes.get("/todo", actions.getTodoAction);
-routes.get("/todos/:id", actions.getTodoAction);
-routes.post("/todos", actions.addTodoAction);
-routes.patch("/todos/:id/toggle", actions.toggleTodoAction);
-routes.patch("/todos/:id/update", actions.updateTodoAction);
-routes.delete("/todos/:id/remove", actions.removeTodoAction);
+routes.get(
+  "/",
+  () =>
+    new Response(JSON.stringify(Deno.version, null, 2), {
+      headers: {
+        "content-type": "application/json",
+      },
+    }),
+);
+// routes.get("/", actions.rootAction);
+// routes.get("/routes", actions.routesAction);
+// routes.get("/todos", actions.listTodosAction);
+// routes.get("/todo", actions.getTodoAction);
+// routes.get("/todos/:id", actions.getTodoAction);
+// routes.post("/todos", actions.addTodoAction);
+// routes.patch("/todos/:id/toggle", actions.toggleTodoAction);
+// routes.patch("/todos/:id/update", actions.updateTodoAction);
+// routes.delete("/todos/:id/remove", actions.removeTodoAction);
