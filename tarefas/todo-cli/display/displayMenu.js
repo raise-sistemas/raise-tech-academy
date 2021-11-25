dc928e36b9d2d5373b317f230f4c76e457178d83
Otávio Jr. O. Nunes <OtavioJr08@gmail.com>
2021-11-24 22:49:03 -0300

    Implementar menus Home e Rotas

--- tarefas/todo-cli/display/displayMenu.js
+++ tarefas/todo-cli/display/displayMenu.js
@@ -1,5 +1,7 @@
 import { cliMenu } from "../menus/cliMenu.js";
 import { toggleMenu } from "../menus/toggleMenu.js";
+import { menuRoutes } from '../menus/menuRoutes.js';
+import { menuHome } from '../menus/menuHome.js';
 
 // Cada entrada do menu é um array com o título da entrada na posição 0 e a referência
 // a função na posição 1
@@ -13,9 +15,9 @@ import { toggleMenu } from "../menus/toggleMenu.js";
 */
 export async function displayMenu() {
   const menu = [
-    //entradas de teste, o primeiro a adicionar as funções pode remover os foo
+    ["Rotas", menuRoutes], // Lista todas as rotas disponíveis
+    ["Home", menuHome], // Exibe o conteúdo da Home
     ["Toggle tarefa", toggleMenu], // Add função.
-    ["foo2", () => console.log("foo2")],
   ];
 
   while (await cliMenu("-=<[ TO-DO Menu ]>=-", menu));
