4cd4065c54e332c8c9e1da528d6b6e7ae4a91868
Sabrina Suzuki <sabrinasuzuki@yahoo.com>
2021-11-24 23:31:01 -0300

    removendo foo

--- tarefas/todo-cli/display/displayMenu.js
+++ tarefas/todo-cli/display/displayMenu.js
@@ -1,12 +1,9 @@
 import { cliMenu } from "../menus/cliMenu.js";
 import { toggleMenu } from "../menus/toggleMenu.js";
-<<<<<<< HEAD
 import { updateTodo } from "../menus/updateMenu.js";
-=======
 import { removeMenu } from "../menus/removeMenu.js";
 import { menuRoutes } from '../menus/menuRoutes.js';
 import { menuHome } from '../menus/menuHome.js';
->>>>>>> 7a5c702c7cfd5418b3ebdaa04804f9187bb2e550
 
 // Cada entrada do menu é um array com o título da entrada na posição 0 e a referência
 // a função na posição 1
@@ -21,16 +18,13 @@ import { menuHome } from '../menus/menuHome.js';
 export async function displayMenu() {
   const menu = [
     //entradas de teste, o primeiro a adicionar as funções pode remover os foo
-<<<<<<< HEAD
+
     ["Toggle tarefa", toggleMenu], // Add função.
-    ["foo2", () => console.log("foo2")],
-    ["Update Todo", updateTodo]
-=======
+    ["Update Todo", updateTodo],
     ["Toggle tarefa", toggleMenu],
     ["Remove tarefa", removeMenu],
     ["Rotas", menuRoutes], // Lista todas as rotas disponíveis
     ["Home", menuHome], // Exibe o conteúdo da Home
->>>>>>> 7a5c702c7cfd5418b3ebdaa04804f9187bb2e550
   ];
 
   while (await cliMenu("TO-DO Menu", menu));
