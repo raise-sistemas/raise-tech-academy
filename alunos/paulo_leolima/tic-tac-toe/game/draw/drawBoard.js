71f44eddd47278126b44a9e0bb4838147ed2bf5a
Leonardo Lima Ribeiro <leonardollr2001@gmail.com>
2021-11-25 19:57:54 -0300

    Alterando teclas do board

--- alunos/paulo_leolima/tic-tac-toe/game/draw/drawBoard.js
+++ alunos/paulo_leolima/tic-tac-toe/game/draw/drawBoard.js
@@ -1,9 +1,9 @@
 export function drawBoard(board) {
   console.log(`
-   ${board[6]}  │  ${board[7]}  │  ${board[8]}
+   ${board[0]}  │  ${board[1]}  │  ${board[2]}
  ─────┼─────┼─────
    ${board[3]}  │  ${board[4]}  │  ${board[5]}
  ─────┼─────┼─────
-   ${board[0]}  │  ${board[1]}  │  ${board[2]}
+   ${board[6]}  │  ${board[7]}  │  ${board[8]}
 `);
 }
