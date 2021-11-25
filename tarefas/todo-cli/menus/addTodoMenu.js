b95e6787e5fb6eba7ccd9a09b2910461edae11fc
Patrick Melo <patrickabimael@yahoo.com.br>
2021-11-25 08:40:48 -0300

    padronizando url no addTodoMenu

--- tarefas/todo-cli/menus/addTodoMenu.js
+++ tarefas/todo-cli/menus/addTodoMenu.js
@@ -1,7 +1,8 @@
+import { BASE_URL } from '../env.js'
 export async function addTodoMenu() {
   const task = prompt('Digite a tarefa que deseja adicionar: ')
   try {
-    const response = await fetch('http://localhost:8000/todos', {
+    const response = await fetch(`${BASE_URL}todos`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
