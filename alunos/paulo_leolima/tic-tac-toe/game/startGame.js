32f68c7ab5d4e1ff99f2d120601ac7045e239715
felipelp121 <felipelopes.serra14@gmail.com>
2021-11-26 03:54:21 -0300

    removendo imports desnecessários

--- alunos/paulo_leolima/tic-tac-toe/game/startGame.js
+++ alunos/paulo_leolima/tic-tac-toe/game/startGame.js
@@ -1,15 +1,7 @@
-import { EMPTY, BOARD, PLAYER1, PLAYER2 } from "./utils/basicElements.js";
+import { BOARD, PLAYER1} from "./utils/basicElements.js";
 import { playerOneScore, playerTwoScore } from "./score/setScore.js";
-import { congratulations } from "./congratulations.js";
-import { isBoardFull } from "./board/isBoardFull.js";
 import { drawScreen } from "./draw/drawScreen.js";
-import { continueGame } from "./continueGame.js";
-import { winnerCheck } from "./winnerCheck.js";
-import { setScore } from "./score/setScore.js";
-import { replayGame } from "./replayGame.js";
 import { gameInput } from "./gameInput.js";
-import { endGame } from "./endGame.js";
-import {isFutureDraw} from "./isFutureDraw.js";
 import {iaPlayer} from "./iaPlayer.js";
 import { yourTurn } from "./yourTurn.js";
 import {roundActions} from "./roundActions.js";
@@ -20,7 +12,7 @@ export function startGame() {
   let pos;
 
   let pcPlayer = +prompt("Digite 1 para jogar sozinho ou 2 para jogar com um amigo!");
-
+  
 
   do {
     drawScreen(BOARD, playerOneScore, playerTwoScore);
