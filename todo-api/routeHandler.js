4329441d90a542c2731762bae9b69b685f0b8452
Neves <marcos.neves@gmail.com>
2021-11-26 08:51:07 -0300

    Add Cors

--- todo-api/routeHandler.js
+++ todo-api/routeHandler.js
@@ -2,9 +2,25 @@ import { routes } from "./routes.js";
 import { jsonResponse } from "./utils/jsonResponse.js";
 import { notFoundResponse } from "./utils/notFoundResponse.js";
 
+function addCors(headers) {
+  headers.append("Access-Control-Allow-Origin", "*");
+  headers.append("Access-Control-Allow-Methods", "*");
+  headers.append("Access-Control-Allow-Headers", "*");
+  headers.append("access-control-max-age", "86400");
+}
+
 export async function routeHandler(request) {
+  console.log(request.method, request.url);
   const { handler, params } = routes.match(request);
   if (!handler) return notFoundResponse(request);
+
   const payload = await handler(request, params);
-  return payload instanceof Response ? payload : jsonResponse(payload);
+
+  const response = payload instanceof Response
+    ? payload
+    : jsonResponse(payload);
+
+  addCors(response.headers);
+
+  return response;
 }
--- todo-api/routes.js
+++ todo-api/routes.js
@@ -3,6 +3,11 @@ import * as actions from "./actions.js";
 
 export const routes = new Router();
 
+function handleCors() {
+  return new Response(null, { status: 204 });
+}
+
+routes.options("/*", handleCors);
 routes.get("/", actions.rootAction);
 routes.get("/routes", actions.routesAction);
 routes.get("/todos", actions.listTodosAction);
