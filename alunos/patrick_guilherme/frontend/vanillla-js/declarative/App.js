d980da5d287cdf8da5d1089e1338648c5a64fa4c
Patrick Melo <patrickabimael@yahoo.com.br>
2021-11-29 15:56:42 -0300

    copia o repositório frontend para a pasta da dupla

---
+++ alunos/patrick_guilherme/frontend/vanillla-js/declarative/App.js
@@ -0,0 +1,27 @@
+import { ListTodos } from "./ListTodos.js";
+
+export function App({ todos }) {
+  const activeTodos = todos.filter((todo) => !todo.completed);
+  const qtyActiveTodos = activeTodos.length;
+  const completedTodos = todos.filter((todo) => todo.completed);
+  const qtyCompletedTodos = completedTodos.length;
+
+  return `
+    <h1>Lista de Compras</h1>
+
+    <h2>Produtos para Comprar (${qtyActiveTodos})</h2>
+    <ol>
+      ${ListTodos({ todos: activeTodos })}
+      <li>
+        <input type="text" onKeyPress="newTodoOnKeyPress(event)" />
+      </li>
+    </ol>
+
+    <hr />
+
+    <h2>Produtos Comprados (${qtyCompletedTodos})</h2>
+    <ol>
+      ${ListTodos({ todos: completedTodos })}
+    </ol>
+  `;
+}
