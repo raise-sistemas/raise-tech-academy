4a6b754e37f738c0795f39a1ff089dc1012bcbdc
senhordrio <adriosilvao@gmail.com>
2021-12-03 09:40:17 -0300

    chore: adds vanilla routing

---
+++ frontend/vanilla-react-routing/src/pages/Settings.js
@@ -0,0 +1,10 @@
+import { onNavigate } from '../utils/onNavigate';
+
+export function Settings() {
+  return (
+    <>
+      <h1>Settings</h1>
+      <button onClick={() => onNavigate('/')}>Voltar</button>
+    </>
+  );
+}
---
+++ frontend/vanilla-react-routing/src/pages/index.js
@@ -0,0 +1,3 @@
+export * from './Dashboard';
+export * from './Settings';
+export * from './Profile';
