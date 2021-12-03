e2f9d945272c7f30853e3b951b90f525644d50c7
Matheus Matsukawa <matheusmatsu@gmail.com>
2021-12-03 20:42:11 -0300

    add custom hook

--- alunos/leonardo_matheus/vanilla-react-routing/src/hooks/useRouting.js
+++ alunos/leonardo_matheus/vanilla-react-routing/src/hooks/useRouting.js
@@ -1,1 +1,13 @@
-export function useRouting() {}
+import { useState, useEffect } from 'react';
+
+export function useRouting() {
+  const [path, setPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    window.addEventListener('popstate', () => {
+      setPath(window.location.pathname);
+    });
+  });
+
+  return path;
+}
