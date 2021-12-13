4b59e58c0d1ef0b6ce334aac393d8a2a0772610c
Matheus Matsukawa <matheusmatsu@gmail.com>
2021-12-13 16:50:41 -0300

    add adress parse method

--- projetos/06_ms_nfe/parser/parse.js
+++ projetos/06_ms_nfe/parser/parse.js
@@ -2,15 +2,17 @@ import { minify } from './minify.js'
 import { chunkRaw } from './chunkRaw.js'
 import { companyName } from './chunks/companyName.js'
 import { cnpj } from './chunks/cnpj.js'
+import { adress } from './chunks/adress.js'
 
 export const parse = (nfe) => {
   const min = minify(nfe);
   const chunkedNfe = chunkRaw(min);
-
+  
   const parsed = {
     company: {
       name: companyName(chunkedNfe.company),
-      cnpj: cnpj(chunkedNfe.company)
+      cnpj: cnpj(chunkedNfe.company),
+      adress: adress(chunkedNfe.company),
     }
   }
 
