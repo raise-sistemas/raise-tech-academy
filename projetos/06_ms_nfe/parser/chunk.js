39fd61be8e5bc9b8d8102607706f2b0a3d92e321
Matheus Matsukawa <matheusmatsu@gmail.com>
2021-12-13 14:39:15 -0300

    add string chunk method

---
+++ projetos/06_ms_nfe/parser/chunk.js
@@ -0,0 +1,11 @@
+import companyPattern from '../stringPatterns/companyPattern.js'
+import purchaseInfoPattern from '../stringPatterns/purchaseInfoPattern.js'
+import purchaseListPattern from '../stringPatterns/purchaseListPattern.js' 
+
+export const chunk = (str) => {
+  return {
+    company: companyPattern.exec(str)[1],
+    purchaseInfo: purchaseInfoPattern.exec(str)[1],
+    purchaseList: purchaseListPattern.exec(str)[1],
+  }
+};
