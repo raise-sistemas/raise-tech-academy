29cf495434930778190ca5a93b3590c7ee560916
Paulo Jr. Niche <pjniche@gmail.com>
2021-12-14 17:09:40 -0300

    add async / await on tests

--- projetos/05_ms_domain-mx-validator/test/isDomainMxValid.test.js
+++ projetos/05_ms_domain-mx-validator/test/isDomainMxValid.test.js
@@ -1,12 +1,12 @@
 import { assertEquals } from "https://deno.land/std@0.114.0/testing/asserts.ts";
 import { isDomainMxValid } from "../src/isDomainMxValid.js";
 
-Deno.test("Entrada de domínio válida", () => {
+Deno.test("Entrada de domínio válida", async () => {
   const domain = "google.com";
-  assertEquals(isDomainMxValid(domain), true);
+  assertEquals(await isDomainMxValid(domain), true);
 });
 
-Deno.test("Entrada de domínio inválida", () => {
+Deno.test("Entrada de domínio inválida", async () => {
   const domain = "google.google";
-  assertEquals(isDomainMxValid(domain), false);
+  assertEquals(await isDomainMxValid(domain), false);
 });
