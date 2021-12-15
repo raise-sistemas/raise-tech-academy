dac81fb46dd24dbe8dba48a269ddf1f154b35749
Leoguarana <leonardoguarana@id.uff.br>
2021-12-15 06:46:26 -0300

    develop run

--- projetos/02_game_batalha-naval/lab/batalha-naval-console/game/run.js
+++ projetos/02_game_batalha-naval/lab/batalha-naval-console/game/run.js
@@ -1,11 +1,15 @@
 import { changePlayerView, view} from "../display/index.js"
-
-
+import { setShot } from "./setShot.js"
+import { readInputShot } from "./readInput.js"
 
 export function run(game){
-  while (game.status)
-  for (const player of game.players){
-    changePlayerView(player)
-    view(player)
-  }  
+  while (game.status){
+    let lastShot;
+    for (const player of game.players){
+      changePlayerView(player)
+      view(player)
+      setShot(lastShot,player,game.players)
+      lastShot = readInputShot(prompt)
+    }  
+  }
 }
