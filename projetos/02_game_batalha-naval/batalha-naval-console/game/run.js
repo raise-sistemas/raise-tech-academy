3454507ed38e7c82cc224f400260e73093143a74
Leoguarana <leonardoguarana@id.uff.br>
2021-12-16 07:20:10 -0300

    complete run

--- projetos/02_game_batalha-naval/batalha-naval-console/game/run.js
+++ projetos/02_game_batalha-naval/batalha-naval-console/game/run.js
@@ -1,15 +1,30 @@
 import { changePlayerView, view} from "../display/index.js"
 import { setShot } from "./setShot.js"
-import { readInputShot } from "./readInput.js"
+import { readInputCoordinate } from "./readInput.js"
+import { setEnemyShot } from "./setEnemyShot.js"
+import { getEnemy } from "./getEnemy.js"
+import { isWinner } from "./isWinner.js"
 
 export function run(game){
+  let lastShot;
+
   while (game.status){
-    let lastShot;
+
     for (const player of game.players){
+      if(game.status){
+      setEnemyShot(lastShot,player.ships,player.grid)
+
       changePlayerView(player)
       view(player)
+
+      lastShot = readInputCoordinate(prompt)
       setShot(lastShot,player,game.players)
-      lastShot = readInputShot(prompt)
-    }  
+
+      game.status = !isWinner(getEnemy(game.players,player).ships)
+      game.Winner = game.players.indexOf(player)
+      }
+    }
+  
   }
+  return game
 }
