01951249c9e69c2575ef624629b21cf371e6ff12
Leonardo Lima Ribeiro <leonardollr2001@gmail.com>
2021-12-16 00:55:06 -0300

    app amigo-secreto

---
+++ projetos/11_app_amigo-secreto/src/App.js
@@ -0,0 +1,13 @@
+import Home from "./components/Home";
+import Cadastrar from "./components/Cadastrar";
+function App() {
+  return (
+    <div>
+      <Home /> 
+      <Cadastrar />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ projetos/11_app_amigo-secreto/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Home(props){
+
+
+    return (    
+        <div>
+            <h1>AMIGO SECRETO</h1>
+                <div class='button'> 
+                    <a href="index.html" class="myButton">insira o <br /> seu nome</a>
+                    <a href="index.html" class="myButton2">revelar nome sorteado</a>
+                </div>
+        </div>
+    );
+}
