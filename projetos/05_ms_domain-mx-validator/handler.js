1adf383b2774fe9cb473e9f3bd838cb0831dfca8
Paulo Jr. Niche <pjniche@gmail.com>
2021-12-16 14:10:00 -0300

    add Matheus Matsukawa suggestions.

--- projetos/05_ms_domain-mx-validator/handler.js
+++ projetos/05_ms_domain-mx-validator/handler.js
@@ -3,6 +3,14 @@ import { isDomainMxValid } from "./src/isDomainMxValid.js";
 export async function handler(Request) {
   const url = new URL(Request.url);
   const domain = url.searchParams.get("mx");
+
+  if (!domain) {
+    return new Response(
+      "Adicionar um parâmetro mx válido ao fim da URL. Ex: ?mx=domínio",
+      { status: 406 }
+    );
+  }
+
   const isValid = await isDomainMxValid(domain);
-  return new Response(isValid);
+  return new Response(isValid, { status: 200 });
 }
