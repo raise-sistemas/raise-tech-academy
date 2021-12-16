c8cebe0394135d2da739184e3eb8371a4253dc99
Paulo Jr. Niche <pjniche@gmail.com>
2021-12-16 12:32:22 -0300

    add deno serve

--- projetos/05_ms_domain-mx-validator/src/isDomainMxValid.js
+++ projetos/05_ms_domain-mx-validator/src/isDomainMxValid.js
@@ -1,16 +1,10 @@
 export async function isDomainMxValid(domain) {
   try {
-    // mxDomains recebe um array de objetos MX, caso exista.
     const mxDomains = await Deno.resolveDns(domain, "MX");
-    // mxDom recebe o primeiro objeto MX válido, caso exista.
     const mxDom = await Promise.any(mxDomains);
-    // retorna o ip do objeto MX, caso exista.
     await Deno.resolveDns(mxDom.exchange, "A");
-    // retorna true em caso de sucesso.
     return true;
-  } catch (e) {
-    // caso alguma operação falhe, retorna false.
-    console.log(e);
+  } catch (_) {
     return false;
   }
 }
