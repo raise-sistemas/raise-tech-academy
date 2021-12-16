51287730c23102f56a9b573b98a62f61e897baea
Paulo Jr. Niche <pjniche@gmail.com>
2021-12-16 13:12:44 -0300

    handler ok!

--- projetos/05_ms_domain-mx-validator/handler.js
+++ projetos/05_ms_domain-mx-validator/handler.js
@@ -1,6 +1,8 @@
 import { isDomainMxValid } from "./src/isDomainMxValid.js";
 
-export async function handler(Request) { // falta tratar o request.
-  const isValid = await isDomainMxValid(Request);
+export async function handler(Request) {
+  const url = new URL(Request.url);
+  const domain = url.searchParams.get("mx");
+  const isValid = await isDomainMxValid(domain);
   return new Response(isValid);
 }
