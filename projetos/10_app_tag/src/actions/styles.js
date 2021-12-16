153b5e940d8bb677570d7b8a47cfc6205807b8c8
senhordrio <adriosilvao@gmail.com>
2021-12-16 09:51:50 -0300

    chore: adiciona sugestões de melhoria Felipe

--- projetos/10_app_tag/src/actions/styles.js
+++ projetos/10_app_tag/src/actions/styles.js
@@ -1,4 +1,3 @@
-
-const body = document.getElementsByTagName("body");
+const body = document.getElementsByTagName('body');
 if (body.classList) body.classList.add('bg-escuro');
-else body[0].className = ('bg-escuro');
+else body[0].className = 'bg-escuro';
--- projetos/10_app_tag/src/components/Home.jsx
+++ projetos/10_app_tag/src/components/Home.jsx
@@ -1,30 +1,36 @@
-import React from "react";
+import React from 'react';
 
-
-export default function Home(props){
-
-    
-
-    if (props.data){
-
-        return(
-
-            <main className="container">
-                {props.data.map((tag, index) =>          
-                    <div key={index} className="bg-roxo card" onClick={() => {navigator.clipboard.writeText(tag.info)}}>
-                        <button className="w500">X</button>
-                        <span>{tag.title}</span>
-                    </div>
-                )}
-            </main>
-        )
-
-
-    }
-
+export default function Home(props) {
+  //FIXME Evitar utilizar if() no jsx, e utilizar operador ternário
+  //https://pt-br.reactjs.org/docs/conditional-rendering.html#inline-if-else-with-conditional-operator
+  //{props.data ? <main/> : <div/>}
+  if (props.data) {
     return (
-        <div className="container t20 lh200">
-            <h2>Não há tags a serem mostradas aqui, <br />Cadastre uma nova tag</h2>
-        </div>
+      //FIXME Essa main pode virar um componente separado e ser importado aqui
+      <main className='container'>
+        {props.data.map((tag, index) => (
+          <div
+            key={index}
+            className='bg-roxo card'
+            onClick={() => {
+              navigator.clipboard.writeText(tag.info);
+            }}
+          >
+            <button className='w500'>X</button>
+            <span>{tag.title}</span>
+          </div>
+        ))}
+      </main>
     );
+  }
+
+  return (
+    //FIXME Essa div pode virar um componente separado e ser importado aqui
+    <div className='container t20 lh200'>
+      <h2>
+        Não há tags a serem mostradas aqui, <br />
+        Cadastre uma nova tag
+      </h2>
+    </div>
+  );
 }
