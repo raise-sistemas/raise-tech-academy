aefb838628d8276e858447c46639d30a5765d60f
Leoguarana <leonardoguarana@id.uff.br>
2021-12-16 07:24:53 -0300

    remove useless variable

--- projetos/02_game_batalha-naval/batalha-naval-console/game/start/getShipLib/isRepeatedCoordinate.js
+++ projetos/02_game_batalha-naval/batalha-naval-console/game/start/getShipLib/isRepeatedCoordinate.js
@@ -1,10 +1,9 @@
 export function isRepeatedCoordinate(coordinate, ships){
-  let answer = false;
   for(const ship in ships){
       if(ships[ship].coordinates.find(element => element === coordinate)){
         console.log(`colindido com o barco ${ships[ship].name} na coordenada ${coordinate}`)
-        answer = true
+        return true
       }
   }
-  return answer
+  return false
 }
