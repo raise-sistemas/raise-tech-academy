d36d627ae44dfd1f7c08c4673b8e64c5802c79dd
Leoguarana <leonardoguarana@id.uff.br>
2021-12-16 07:20:48 -0300

    add set shot

--- projetos/02_game_batalha-naval/batalha-naval-console/game/setShot.js
+++ projetos/02_game_batalha-naval/batalha-naval-console/game/setShot.js
@@ -1,14 +1,15 @@
 
 import { changeEmoji } from "../utils/changeEmoji.js"
 import { isRepeatedCoordinate } from "./start/getShipLib/isRepeatedCoordinate.js"
-import { getEnemyShips } from "./getEnemyShips.js"
+import { getEnemy } from "./getEnemy.js"
 
 export function setShot(lastShot,player,players){
-  const enemyShips = getEnemyShips(players,player)
+  const enemyShips = getEnemy(players,player).ships
   if(lastShot){
     if(isRepeatedCoordinate(lastShot,enemyShips)){
     changeEmoji(player.enemyGrid,'💥',lastShot)
-    }
+    }else{
     changeEmoji(player.enemyGrid,'🟦',lastShot)
+    }
   }
 }
