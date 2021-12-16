5a87a4dddbf3af9984f26f1bdbdde07e8701d417
Leoguarana <leonardoguarana@id.uff.br>
2021-12-16 07:22:22 -0300

    add winner index

--- projetos/02_game_batalha-naval/batalha-naval-console/game/start/newGame.js
+++ projetos/02_game_batalha-naval/batalha-naval-console/game/start/newGame.js
@@ -2,7 +2,8 @@ import { newPlayer } from "./newPlayer.js"
 
 export function newGame(){
   return {
+    winner : 0,
     status : true,
-    players : [newPlayer(),newPlayer()]
+    players : [newPlayer(1), newPlayer(2)]
   } 
 }
