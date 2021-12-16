4b8eb6c7b7d9ad0b9511fd80f4e0d121b95ae1e7
senhordrio <adriosilvao@gmail.com>
2021-12-16 15:55:29 -0300

    chore: adiciona FIXMEs Nicole

--- projetos/09_app_km-litro/kilitro/src/App.js
+++ projetos/09_app_km-litro/kilitro/src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
- import Form from "./components/input.js"
+import Form from './components/input.js';
 
 function App() {
+  //FIXME o <Form/> estava como descendente de p, e isso não é permitido, apesar de rodar, p não pode ter filhos.
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          < Form />
-        </p>
+    <div className='App'>
+      <header className='App-header'>
+        {/* <p>
+          Edit <code>src/App.js</code> and save to reload. */}
+        <Form />
+        {/* </p> */}
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
--- projetos/09_app_km-litro/kilitro/src/components/input.js
+++ projetos/09_app_km-litro/kilitro/src/components/input.js
@@ -1,36 +1,64 @@
 // import Autonomy from "./autonomy.js"
-import { useState } from 'react'
+import { useState } from 'react';
 
-function Form () {
+//FIXME Um bom padrão é utilizar nome dos arquivos que são componentes com letra maiúscula, e com nomes iguais ao da função.
+//no caso, Form.js.
+//
+//
+//
+//
+//
+//
 
-    function Autonomy(e) {
-        e.preventDefault();
-        console.log(km);
-        console.log(liter);
-        console.log(newKm);
-        const autonomy = (newKm-km)/liter;
-        console.log(autonomy.toFixed(1));
-        const a = localStorage.setItem('autonomy', autonomy);
+function Form() {
+  function Autonomy(e) {
+    e.preventDefault();
+    console.log(km);
+    console.log(liter);
+    console.log(newKm);
+    const autonomy = (newKm - km) / liter;
+    console.log(autonomy.toFixed(1));
+    const a = localStorage.setItem('autonomy', autonomy);
 
-        //var valorRecuperado = Number(localStorage.getItem(chave))
-    }
-    
-    const [km, setKm] = useState();
-    const [liter, setLiter] = useState();
-    const [newKm, setNewKm] = useState();
+    //var valorRecuperado = Number(localStorage.getItem(chave))
+  }
 
-    return (
-        <div>
-            <form onSubmit={Autonomy}>
-                <input id ="km" type='number' placeholder='Insira a quilometragem atual' onChange={(e) => setKm(e.target.value)}/>
-                {/* <input id="price" type='number' placeholder='Insira o preço da gasolina'/> */}
-                <input id="newKm" type='number' placeholder='Insira a quilometragem após' onChange={(e) => setNewKm(e.target.value)}/>
-                <input id="liter" type='number' placeholder='Insira a quantidade de litros abastecido' onChange={(e) => setLiter(e.target.value)}/>
-                <button id='autonomy' type="submit">Cadastrar</button>
-            </form>
-            
-        </div>      
-    )
+  const [km, setKm] = useState();
+  const [liter, setLiter] = useState();
+  const [newKm, setNewKm] = useState();
+
+  //FIXME No form aqui você pode adicionar labels aos inputs, pra melhorar para o usuário. Deixei um exemplo.
+
+  return (
+    <div>
+      <form onSubmit={Autonomy}>
+        <label htmlFor='km'>Quilometragem</label>
+        <input
+          name='km'
+          id='km'
+          type='number'
+          placeholder='Insira a quilometragem atual'
+          onChange={(e) => setKm(e.target.value)}
+        />
+        {/* <input id="price" type='number' placeholder='Insira o preço da gasolina'/> */}
+        <input
+          id='newKm'
+          type='number'
+          placeholder='Insira a quilometragem após'
+          onChange={(e) => setNewKm(e.target.value)}
+        />
+        <input
+          id='liter'
+          type='number'
+          placeholder='Insira a quantidade de litros abastecido'
+          onChange={(e) => setLiter(e.target.value)}
+        />
+        <button id='autonomy' type='submit'>
+          Cadastrar
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Form;
