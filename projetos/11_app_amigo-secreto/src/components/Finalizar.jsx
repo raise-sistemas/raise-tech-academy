9596f101547594071ca9308a7271b50d32694e21
Leonardo Lima Ribeiro <leonardollr2001@gmail.com>
2021-12-17 04:16:45 -0300

    adicionando funcionalidades

---
+++ projetos/11_app_amigo-secreto/src/components/Finalizar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Finalizar() {
+    return (
+        <div>
+            
+            <div>
+                <button class="botao-finalizar">
+                <a href="index.html">Finalizar</a>
+                </button>
+            </div>
+
+      </div>
+    )
+
+}
--- projetos/11_app_amigo-secreto/src/components/Home.jsx
+++ projetos/11_app_amigo-secreto/src/components/Home.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 
-export default function Home(props){
+export default function Home(props) {
 
 
     return (    
         <div>
             <h1>AMIGO SECRETO</h1>
-                <div class='button'> 
-                    <a href="index.html" class="myButton">insira o <br /> seu nome</a>
-                    <a href="index.html" class="myButton2">revelar nome sorteado</a>
+                <div>
+                    <button class='inserir-revelar'>
+                    Inserir o nome dos participantes
+                    </button>
                 </div>
+
+                <div>
+                    <button class='inserir-revelar'>
+                    Revelar nome sorteado
+                    </button>
+                </div>
+                
         </div>
     );
 }
