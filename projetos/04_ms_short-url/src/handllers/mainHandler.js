0f4e91c88cf834795cd90df123a7d8c2f71339a3
Otávio Jr. O. Nunes <OtavioJr08@gmail.com>
2021-12-17 00:15:55 -0300

    Corrigir funçao apiKeyRequest

--- projetos/04_ms_short-url/src/handllers/mainHandler.js
+++ projetos/04_ms_short-url/src/handllers/mainHandler.js
@@ -1,12 +1,12 @@
 import { routeHandler } from "./routeHandler.js";
-import { authorizeRequest } from "../utils/authorizeRequest.js";
+import { apiKeyResquest } from "../utils/apiKeyResquest.js";
 
 export async function mainHandler(request) {
   console.log(request.method, request.url);
   
   if(request.method === 'POST')
-    authorizeRequest(request);
-  
+    apiKeyResquest(request);
+
   const response = await routeHandler(request);
 
   return response;
--- projetos/04_ms_short-url/src/utils/apiKeyResquest.js
+++ projetos/04_ms_short-url/src/utils/apiKeyResquest.js
@@ -1,7 +1,7 @@
 const KEY = "ap147ldokdmra00kmbrgkdk2mgja75p1km5gnvjdnefhhfkdna5asd2lwlm6fkaweiuitnbl";
 
-export function authorizeRequest({headers}) {
-  const authorization = headers.get("authorization")?.split(' ')[1];
+export function apiKeyResquest({headers}) {
+  const authorization = headers.get("key");
 
   if (!(authorization === KEY))
     throw "Acesso Negado";
