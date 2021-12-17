f0fff4eb1c37665b7d3a2ef83255003442d2e33a
Otávio Jr. O. Nunes <OtavioJr08@gmail.com>
2021-12-17 00:28:13 -0300

    Implementar cors

--- projetos/04_ms_short-url/src/handllers/mainHandler.js
+++ projetos/04_ms_short-url/src/handllers/mainHandler.js
@@ -1,5 +1,6 @@
 import { routeHandler } from "./routeHandler.js";
 import { apiKeyResquest } from "../utils/apiKeyResquest.js";
+import { addCors } from "../utils/addCors.js";
 
 export async function mainHandler(request) {
   console.log(request.method, request.url);
@@ -8,6 +9,8 @@ export async function mainHandler(request) {
     apiKeyResquest(request);
 
   const response = await routeHandler(request);
+  
+  addCors(response.headers);
 
   return response;
 }
