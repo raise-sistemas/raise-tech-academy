d827084c9e38f1c240ec73de69a619d78661bbd5
João Victor Albuquerque <93137253+jvoaojvictor@users.noreply.github.com>
2021-12-18 20:44:24 -0300

    Modularizando o código

---
+++ projetos/01_game_crosswords/game/newPlay.js
@@ -0,0 +1,18 @@
+import foundWords from './board/foundWords.js';
+import { showPlays } from "./display/showPlays.js";
+import { searchWord } from "./validation/searchWord.js";
+import { displayBoard } from "./display/displayBoard.js";
+import { modifyBoard } from "./board/modify/modifyBoard.js";
+import { checkCorrectWord } from "./validation/checkCorrectWord.js";
+
+export function newPlay(input, matriz) {
+    let wordPositions = input.match(/[a-z]+|[^a-z]+/gi);
+    let word = searchWord(wordPositions, matriz);
+    let amountWordsFound = foundWords.length;
+    checkCorrectWord(word);
+    if (foundWords.length > amountWordsFound) matriz = modifyBoard(matriz, wordPositions);
+    displayBoard(matriz);
+    if (foundWords.length === amountWordsFound) showMessage('❌ Ops.. palavra incorreta!');
+    showPlays(foundWords);
+    return matriz;
+}
