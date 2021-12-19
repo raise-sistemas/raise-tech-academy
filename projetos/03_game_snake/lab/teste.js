42c23ebcd5de9e4d04ec7d88ff8059a2813ec166
Patrick Melo <patrickabimael@yahoo.com.br>
2021-12-18 22:17:18 -0300

    refatora função que avisa se tem cenoura imitente

--- projetos/03_game_snake/lab/teste.js
+++ projetos/03_game_snake/lab/teste.js
@@ -12,16 +12,19 @@ import { isUpper, carrotFinder, bunnyFinder } from '../main/utils/index.js'
 import { isCarrotUpper } from '../main/utils/isCarrotUpper.js'
 import { outOfRange } from '../main/utils/outOfRange.js'
 
-const bunnyLine = bunnyFinder(gridTest)[0]
-console.log(bunnyLine)
+// const bunnyLine = bunnyFinder(gridTest)[0]
+const carrot = carrotFinder(gridTest)
 
-console.log(carrotFinder(gridTest))
-console.log(bunnyFinder(gridTest))
-// console.log(imminentCarrotUp(gridTest))
+// console.log(bunnyLine - carrotIndex)
+// console.log(carrot)
+// console.log(bunnyFinder(gridTest))
+// console.log(outOfRange(bunnyLine))
+// console.log(bunnyLine)
+// console.log(carrotIndex)
 
-console.log(outOfRange(bunnyLine))
+console.log(imminentCarrotUp(gridTest))
+
 // coordinates[carrotLine][carrotColumn] - bunnyLine == 16
-
 // console.log(imminentCarrotUp(gridTest))
 // console.log(isUpper(gridTest))
 // console.log(isCarrotUpper(gridTest))
