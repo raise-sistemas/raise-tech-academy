f916b2eb431cd249f2c93c9f29ca815fd5fc67a1
Patrick Melo <patrickabimael@yahoo.com.br>
2021-12-18 22:32:50 -0300

    implementa função de gerar nova cenoura ao comer no controle de andar pra cima

--- projetos/03_game_snake/lab/teste.js
+++ projetos/03_game_snake/lab/teste.js
@@ -12,19 +12,5 @@ import { isUpper, carrotFinder, bunnyFinder } from '../main/utils/index.js'
 import { isCarrotUpper } from '../main/utils/isCarrotUpper.js'
 import { outOfRange } from '../main/utils/outOfRange.js'
 
-// const bunnyLine = bunnyFinder(gridTest)[0]
-const carrot = carrotFinder(gridTest)
 
-// console.log(bunnyLine - carrotIndex)
-// console.log(carrot)
-// console.log(bunnyFinder(gridTest))
-// console.log(outOfRange(bunnyLine))
-// console.log(bunnyLine)
-// console.log(carrotIndex)
 
-console.log(imminentCarrotUp(gridTest))
-
-// coordinates[carrotLine][carrotColumn] - bunnyLine == 16
-// console.log(imminentCarrotUp(gridTest))
-// console.log(isUpper(gridTest))
-// console.log(isCarrotUpper(gridTest))
