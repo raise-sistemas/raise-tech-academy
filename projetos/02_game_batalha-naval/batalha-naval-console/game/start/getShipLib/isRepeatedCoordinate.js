27810795c8cb13cbff3981d6cf1571b59c69bdc1
Leoguarana <leonardoguarana@id.uff.br>
2021-12-22 20:02:21 -0300

    refactore

--- projetos/02_game_batalha-naval/batalha-naval-console/game/start/getShipLib/isRepeatedCoordinate.js
+++ projetos/02_game_batalha-naval/batalha-naval-console/game/start/getShipLib/isRepeatedCoordinate.js
@@ -1,6 +1,7 @@
+import { isRepeated } from "../../isRepeated.js"
 export function isRepeatedCoordinate(coordinate, ships){
   for(const ship in ships){
-      if(ships[ship].coordinates.find(element => element === coordinate)){
+      if(isRepeated(coordinate,ships[ship].coordinates)){
         console.log(`colindido com o barco ${ships[ship].name} na coordenada ${coordinate}`)
         return true
       }
