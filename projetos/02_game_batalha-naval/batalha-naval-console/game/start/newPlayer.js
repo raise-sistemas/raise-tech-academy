73635ed3f46002b12edc0d3ed1a07d14c9fd08db
Leoguarana <leonardoguarana@id.uff.br>
2021-12-25 00:50:52 -0300

    remove numbers

--- projetos/02_game_batalha-naval/batalha-naval-console/game/start/newPlayer.js
+++ projetos/02_game_batalha-naval/batalha-naval-console/game/start/newPlayer.js
@@ -10,10 +10,10 @@ export function newPlayer(playerNumber,bot=false) {
     name : getName(playerNumber,prompt,bot),
     ships:{
       aircraftCarriers :newBoat(`porta-aviões`,5),
-      battleships : newBoat(`Couraçado`,4),       //4  
-      Cruisers : newBoat(`Cruzador`,3),          //3  
-      Destroyers : newBoat(`Contratorpedeiro`,2),        //2  
-      Submarines : newBoat(`Submarino`,1)                  //1  
+      battleships : newBoat(`Couraçado`,4),         
+      Cruisers : newBoat(`Cruzador`,3),            
+      Destroyers : newBoat(`Contratorpedeiro`,2),          
+      Submarines : newBoat(`Submarino`,1)                   
     },
     shots: [],
     bot:newBot(bot),
